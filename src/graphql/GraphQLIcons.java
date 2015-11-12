63a1bec40e1052e6132db09764c134ab59a8fdba
Gregor <gregorwbr@gmail.com>
2015-11-12 10:48:23 +0100

    Swap relay icon for correct graphql icon

--- src/graphql/GraphQLIcons.java
+++ src/graphql/GraphQLIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class GraphQLIcons {
-    public static final Icon FILE = IconLoader.getIcon("/graphql/icons/relay.png");
+    public static final Icon FILE = IconLoader.getIcon("/graphql/icons/graphql.png");
 }
